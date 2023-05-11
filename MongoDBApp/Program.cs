0600a2102a0497cb58d94477f18b1e1929108678
Egor Dubrovsky <egordubrovckii@mail.ru>
2023-05-11 10:34:44 +0300

    listing databases

--- MongoDBApp/Program.cs
+++ MongoDBApp/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace MongoDBApp
+﻿using MongoDB.Driver;
+
+namespace MongoDBApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            using (var cursor = await client.ListDatabasesAsync())
+            {
+                var databases = cursor.ToList();
+                foreach (var database in databases)
+                {
+                    Console.WriteLine(database);
+                }
+            }
+
             Console.WriteLine("Hello, World!");
         }
     }
