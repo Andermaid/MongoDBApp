a6ca90a8318d3af4bb8ec2a94fa4f68033af74f1
Egor Dubrovsky <egordubrovckii@mail.ru>
2023-05-16 14:43:04 +0300

    added dataholder class

---
+++ MongoDBApp/DataHolder.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBApp
+{
+    internal class DataHolder
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("data")]
+        public string Data { get; set; }
+    }
+}
